afd5aed21150b2b9fba76009e238aaf1d3f2c3b4
bifot <bifot@bifot.ru>
2019-05-06 20:43:57 +0300

    updated typings

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,5 +1,7 @@
 type callback = (data: object) => any;
 
+declare class Socket {}
+
 declare class UDP {
   public services: object;
   public timeout: number;
@@ -12,10 +14,10 @@ declare class UDP {
 
   public on(event: string, callback: callback): this;
   public emit(event: string, data: object): this;
-  public middleware(): (arg1: any, arg2: any, arg3: any) => void;
+  public middleware(): (...args) => void;
   public ask(event: string, data: object, options?: { attempts?: number }): Promise<void>
   public createSockets(): Promise<void>;
-  public listen(port: number, address?: string): Promise<any>;
+  public listen(port: number, address?: string): Promise<Socket>;
 }
 
 export = UDP;
